508039425e3202c55bea865ef37dcea3d1ec4af6
Park-MinWoo <74492431+Park-MinWoo@users.noreply.github.com>
2022-02-27 22:07:50 +0900

    Delete projects directory

--- projects/딥러닝실제/4주차 과제.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt 
-from matplotlib.image import imread 
-
-img = imread('/Users/park/산업인공지능학과/딥러닝실제/과제/IMG_0071.JPG')
-
-plt.imshow(img)
-plt.show()
